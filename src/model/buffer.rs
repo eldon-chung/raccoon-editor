d7dea96c43b17137b2d53eabc4001e599255fae8
Eldon Chung <eldon.chung.yr@gmail.com>
2020-05-19 18:29:47 +0800

    add basic buffer struct

---
+++ src/model/buffer.rs
@@ -0,0 +1,29 @@
+pub struct Buffer {
+	contents: String
+}
+
+impl Buffer {
+	pub fn new() -> Buffer {
+		Buffer {
+			contents: String::new()
+		}
+	}
+
+	pub fn with_string(string: String) -> Buffer {
+		Buffer {
+			contents: string
+		}
+	}
+
+	pub fn insert(&mut self, idx: usize, ch: char) {
+		self.contents.insert(idx, ch);
+	}
+
+	pub fn insert_str(&mut self, idx: usize, string: &str) {
+		self.contents.insert_str(idx, string);
+	}
+
+	pub fn remove(&mut self, idx: usize) -> char {
+		self.contents.remove(idx)
+	}
+}
