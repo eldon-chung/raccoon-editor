041f06b78823933f02e71003fb39a2cc127fa045
Eldon Chung <eldon.chung.yr@gmail.com>
2020-05-19 21:50:08 +0800

    add view component

---
+++ src/view/mod.rs
@@ -0,0 +1,45 @@
+use std::io;
+
+#[allow(unused_imports)]
+use tui::{
+    backend::{Backend,TermionBackend},
+    layout::{Constraint, Direction, Layout, Alignment},
+    style::{Color, Style, Modifier},
+    widgets::{Block, Borders, List, Paragraph, Text},
+    Terminal,
+};
+use crate::model::app::App;
+
+pub struct View<B: Backend> {
+	terminal: Terminal<B>,
+	// Probably want to include layout details
+	//  here eventually.
+}
+
+impl <B: Backend> View <B> {
+	pub fn new(terminal: Terminal<B>) -> View<B> {
+		View {
+			terminal: terminal
+		}
+	}
+
+	pub fn update_display(&mut self, app: &App) -> Result<(), io::Error> {
+		let text = app.get_text_as_iter(); // Get a copy of the text to be rendered
+		// For now let's not do anything fancy formatting
+		let text: Vec<_> = text.iter().map(|&x| Text::raw(x) )
+						.collect();
+		self.terminal.draw(|mut f| {
+	        let size = f.size();
+	        let block = Paragraph::new(text.iter())
+					    .block(Block::default().title("Paragraph").borders(Borders::ALL))
+					    .style(Style::default().fg(Color::White).bg(Color::Black))
+					    .alignment(Alignment::Left)
+					    .wrap(true);
+	        f.render_widget(block, size);
+    	})?;
+
+    	Ok(())
+	}
+
+
+}
