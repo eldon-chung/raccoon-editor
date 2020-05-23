ff89637ae2cf139b0aa03c081cb95717ea16e158
Eldon Chung <eldon.chung.yr@gmail.com>
2020-05-23 14:04:43 +0800

    fix fencepost errors for remove method

--- src/model/buffer.rs
+++ src/model/buffer.rs
@@ -1,4 +1,5 @@
 use crate::utils::Cursor;
+
 #[allow(dead_code)]
 
 
@@ -61,6 +62,7 @@ pub struct Buffer {
 }
 
 impl Buffer {
+
 	fn get_offsets(string: &str) -> Vec<usize> {
 		// Whelp this looks gross. Probably prettify or rewrite later on
 		let acc = vec![0];
@@ -71,11 +73,10 @@ impl Buffer {
 	}
 
 	pub fn new() -> Buffer {
-		let first_node = BufferNode::new(BufferType::Original, 0, 0, vec![0]);
 		Buffer {
 			original_str: Vec::new(),
 			added_str: Vec::new(),
-			node_list: vec![first_node],
+			node_list: Vec::new(),
 		}
 	}
 
@@ -106,6 +107,16 @@ impl Buffer {
 		// append string to add_str
 		let mut vec_converted = string.chars().collect::<Vec<char>>();
 		self.added_str.append(&mut vec_converted);
+
+		if self.node_list.len() == 0 {
+			cursor.node_idx = 0;
+			cursor.node_offset = offset;
+			cursor.line_idx = num_lines - 1;
+			let last_offset = new_node.last_line_offset();
+			cursor.line_offset = cursor.node_offset - last_offset;
+			self.node_list.push(new_node);
+			return;
+		}
 
 		// split the current node we are on into two
 		let node_to_split = &mut self.node_list[cursor.node_idx];
@@ -166,28 +177,30 @@ impl Buffer {
 		// 	two nodes or in the middle of a single node. both cases are handled
 		//  quite differently
 
+		if self.node_list.len() == 0 {
+			return;
+		}
+
+		if cursor.node_idx == 0 && cursor.node_offset == 0 {
+			return;
+		}
+
 		let mut node_idx = cursor.node_idx;
 		let mut node_offset = cursor.node_offset;
 
-
-		if node_offset == 0 {
-			// if we're at the head of the file there's nothing to do;
-			if node_idx == 0 { return; }
-			// else reference the previous node
+		if node_offset == 0 && node_idx >= 1 {
 			node_idx -= 1;
 			node_offset = self.node_list[node_idx].offset();
 		}
 
 		let from = self.node_list[node_idx].from();
+		let offset = self.node_list[node_idx].offset();
 		let index = self.node_list[node_idx].index();
 		let line_offsets = &mut self.node_list[node_idx].line_offsets();
 		let mut line_offsets: Vec<usize> = line_offsets.drain(..).collect();
 
 		if node_offset == self.node_list[node_idx].offset() {
-			// an edge case is that this is the empty node that appears when
-			// we've just constructed a file. in that case we should return as well
-			if node_idx == 0 &&  node_offset == 0 { return; }
-			// just reduce the offset value by 1 here
+			// just reduce the node offset by 1 here
 			node_offset -= 1;
 			if node_offset == 0 {
 				// node is now empty, just delete it and move the cursor
@@ -207,7 +220,7 @@ impl Buffer {
 			let new_node = BufferNode::new(from, index, node_offset, line_offsets);
 			// in this case the cursor position does not need to be updated?
 			//  unless the cursor was at the end of the entire buffer
-			if cursor.node_idx == self.node_list.size() - 1 {
+			if node_idx == self.node_list.len() - 1 {
 				cursor.node_offset -= 1;
 			}
 			self.node_list.insert(node_idx, new_node);
@@ -215,7 +228,7 @@ impl Buffer {
 		} else {
 			// split the line_offsets based on where the cursor is
 			let drain_from_idx = cursor.line_idx;
-			let left_line_offsets = line_offsets.drain(..=drain_from_idx).collect();
+			let mut left_line_offsets: Vec<usize> = line_offsets.drain(..=drain_from_idx).collect();
 			let node_offset = cursor.node_offset;
 			let mut right_line_offsets: Vec<usize> = line_offsets.drain( .. )
 												.map(|x| x - node_offset)
@@ -223,8 +236,8 @@ impl Buffer {
 			right_line_offsets.insert(0, 0);
 
 			let removed_char = match from {
-				BufferType::Original => self.original_str[index + node_offset],
-				BufferType::Added => self.added_str[index + node_offset],
+				BufferType::Original => self.original_str[index + node_offset - 1],
+				BufferType::Added => self.added_str[index + node_offset - 1],
 			};
 			if removed_char == '\n' {
 				left_line_offsets.pop();
@@ -288,176 +301,5 @@ impl Buffer {
 }
 
 #[cfg(test)]
-mod tests {
-	use super::*;
-
-	#[test]
-	fn get_offsets_none() {
-		let string = String::from("abc");
-		let offsets = Buffer::get_offsets(&string);
-		assert_eq!(offsets, &[0]);
-	}
-
-	#[test]
-	fn get_offsets_first() {
-		let string = String::from("\nabc");
-		let offsets = Buffer::get_offsets(&string);
-		assert_eq!(offsets, &[0, 1]);
-	}
-
-	#[test]
-	fn get_offsets_last() {
-		let string = String::from("abc\n");
-		let offsets = Buffer::get_offsets(&string);
-		assert_eq!(offsets, &[0, 4]);
-	}
-
-	#[test]
-	fn get_offsets_only() {
-		let string = String::from("\n");
-		let offsets = Buffer::get_offsets(&string);
-		assert_eq!(offsets, &[0, 1]);
-	}
-
-	#[test]
-	fn get_offsets_two() {
-		let string = String::from("\nabc\n");
-		let offsets = Buffer::get_offsets(&string);
-		assert_eq!(offsets, &[0, 1, 5]);
-	}
-
-	#[test]
-	fn get_offsets_consecutive() {
-		let string = String::from("\n\n\n");
-		let offsets = Buffer::get_offsets(&string);
-		assert_eq!(offsets, &[0, 1, 2, 3]);
-	}
-
-	#[test]
-	fn insert_char_into_fresh_buffer() {
-		let mut cursor = Cursor::new();
-		let mut buffer = Buffer::new();
-		buffer.insert(&mut cursor, 'a');
-
-		assert_eq!(buffer.original_str, "", "original_str mismatch");
-		assert_eq!(buffer.added_str, "a", "added_str mismatch");
-
-		let node_0 = BufferNode::new(BufferType::Original, 0, 0, vec![0] );
-		let node_1 = BufferNode::new(BufferType::Added, 0, 1, vec![0] );
-		assert_eq!(buffer.node_list, vec![node_0, node_1], "node_list mismatch");
-
-		assert_eq!(cursor.node_idx, 1, "cursor.node_idx mismatch");
-		assert_eq!(cursor.node_offset, 1, "cursor.node_offset mismatch");
-		assert_eq!(cursor.line_idx, 0, "cursor.line_idx mismatch");
-		assert_eq!(cursor.line_offset, 1, "cursor.line_offset mismatch");
-	}
-
-	#[test]
-	fn insert_newl_into_fresh_buffer() {
-		let mut cursor = Cursor::new();
-		let mut buffer = Buffer::new();
-		buffer.insert(&mut cursor, '\n');
-
-		assert_eq!(buffer.original_str, "", "original_str mismatch");
-		assert_eq!(buffer.added_str, "\n", "added_str mismatch");
-
-		let node_0 = BufferNode::new(BufferType::Original, 0, 0, vec![0] );
-		let node_1 = BufferNode::new(BufferType::Added, 0, 1, vec![0, 1] );
-		assert_eq!(buffer.node_list, vec![node_0, node_1], "node_list mismatch");
-
-		assert_eq!(cursor.node_idx, 1, "cursor.node_idx mismatch");
-		assert_eq!(cursor.node_offset, 1, "cursor.node_offset mismatch");
-		assert_eq!(cursor.line_idx, 1, "cursor.line_idx mismatch");
-		assert_eq!(cursor.line_offset, 0, "cursor.line_offset mismatch");
-	}
-
-	#[test]
-	fn insert_newlnewl_into_fresh_buffer() {
-		let mut cursor = Cursor::new();
-		let mut buffer = Buffer::new();
-		let inserted_str = String::from("\n\n");
-		buffer.insert_str(&mut cursor, inserted_str);
-
-		assert_eq!(buffer.original_str, "", "original_str mismatch");
-		assert_eq!(buffer.added_str, "\n\n", "added_str mismatch");
-
-		let node_0 = BufferNode::new(BufferType::Original, 0, 0, vec![0] );
-		let node_1 = BufferNode::new(BufferType::Added, 0, 2, vec![0, 1, 2] );
-		assert_eq!(buffer.node_list, vec![node_0, node_1], "node_list mismatch");
-
-		assert_eq!(cursor.node_idx, 1, "cursor.node_idx mismatch");
-		assert_eq!(cursor.node_offset, 2, "cursor.node_offset mismatch");
-		assert_eq!(cursor.line_idx, 2, "cursor.line_idx mismatch");
-		assert_eq!(cursor.line_offset, 0, "cursor.line_offset mismatch");
-	}
-
-	#[test]
-	fn insert_before_node() {
-		let mut cursor = Cursor::new();
-		let mut buffer = Buffer::with_contents(String::from("abdc\nef"));
-
-		buffer.insert_str(&mut cursor, String::from("1\n2"));
-
-		assert_eq!(buffer.original_str, "abdc\nef", "original_str mismatch");
-		assert_eq!(buffer.added_str, "1\n2", "added_str mismatch");
-
-		let node_0 = BufferNode::new(BufferType::Added, 0, 3, vec![0, 2] );
-		let node_1 = BufferNode::new(BufferType::Original, 0, 7, vec![0, 5] );
-		assert_eq!(buffer.node_list, vec![node_0, node_1], "node_list mismatch");
-
-		assert_eq!(cursor.node_idx, 1, "cursor.node_idx mismatch");
-		assert_eq!(cursor.node_offset, 0, "cursor.node_offset mismatch");
-		assert_eq!(cursor.line_idx, 0, "cursor.line_idx mismatch");
-		assert_eq!(cursor.line_offset, 0, "cursor.line_offset mismatch");
-	}
-
-	#[test]
-	fn insert_after_node() {
-		let mut cursor = Cursor::new();
-		let mut buffer = Buffer::with_contents(String::from("abdc\nef"));
-		cursor.node_idx = 0;
-		cursor.node_offset = 7;
-		cursor.line_idx = 1;
-		cursor.line_offset = 2;
-
-		buffer.insert_str(&mut cursor, String::from("1\n2"));
-
-		assert_eq!(buffer.original_str, "abdc\nef", "original_str mismatch");
-		assert_eq!(buffer.added_str, "1\n2", "added_str mismatch");
-
-		let node_0 = BufferNode::new(BufferType::Original, 0, 7, vec![0, 5] );
-		let node_1 = BufferNode::new(BufferType::Added, 0, 3, vec![0, 2] );
-		assert_eq!(buffer.node_list, vec![node_0, node_1], "node_list mismatch");
-
-		assert_eq!(cursor.node_idx, 1, "cursor.node_idx mismatch");
-		assert_eq!(cursor.node_offset, 3, "cursor.node_offset mismatch");
-		assert_eq!(cursor.line_idx, 1, "cursor.line_idx mismatch");
-		assert_eq!(cursor.line_offset, 1, "cursor.line_offset mismatch");
-	}
-
-	#[test]
-	fn insert_mid_node() {
-		let mut cursor = Cursor::new();
-		let mut buffer = Buffer::with_contents(String::from("abdc\nef\ng"));
-		cursor.node_idx = 0;
-		cursor.node_offset = 5;
-		cursor.line_idx = 1;
-		cursor.line_offset = 0;
-
-		buffer.insert_str(&mut cursor, String::from("1\n2"));
-		//|abdc\n|1\n2|ef\ng
-
-		assert_eq!(buffer.original_str, "abdc\nef\ng", "original_str mismatch");
-		assert_eq!(buffer.added_str, "1\n2", "added_str mismatch");
-
-		let node_0 = BufferNode::new(BufferType::Original, 0, 5, vec![0, 5] );
-		let node_1 = BufferNode::new(BufferType::Added, 0, 3, vec![0, 2] );
-		let node_2 = BufferNode::new(BufferType::Original, 5, 4, vec![0, 3] );
-		assert_eq!(buffer.node_list, vec![node_0, node_1, node_2], "node_list mismatch");
-
-		assert_eq!(cursor.node_idx, 2, "cursor.node_idx mismatch");
-		assert_eq!(cursor.node_offset, 0, "cursor.node_offset mismatch");
-		assert_eq!(cursor.line_idx, 0, "cursor.line_idx mismatch");
-		assert_eq!(cursor.line_offset, 0, "cursor.line_offset mismatch");
-	}
-}
+#[path = "tests/buffer_tests.rs"]
+mod buffer_tests;
