3a38a01671a0a59ca3a25dd21d6ffd765fa669b9
Christian James Welly <chrisjwelly@gmail.com>
2020-06-03 22:19:12 +0800

    Write test for opening and saving files with restricted permissions

--- src/model/tests/app_tests.rs
+++ src/model/tests/app_tests.rs
@@ -1,8 +1,9 @@
 #[cfg(test)]
 mod app_tests {
     use super::super::*;
-    use std::fs::File;
+    use std::fs::{self, File};
     use std::io::{self, Write};
+    use std::os::unix::fs::PermissionsExt;
     use std::path::Path;
     use tempfile::tempdir;
 
@@ -76,6 +77,57 @@ mod app_tests {
 
         dir.close()?;
         Ok(())
+    }
+
+    #[test]
+    #[should_panic(expected = "Permission denied")]
+    fn open_writeonly_file_fail() {
+        // Doesn't return io::Result<()> like other tests because should_panic must return unit
+        // Prepare the files
+        let dir = tempdir().expect("Failed to create tempdir");
+        let file_path = dir.path().join("temp.txt");
+        let file_path_string = file_path.to_string_lossy().into_owned();
+        let file = File::create(file_path.clone()).expect("Failed to create file");
+
+        // Make the file become writeonly
+        let metadata = file.metadata().expect("Failed to get metadata");
+        let mut permissions = metadata.permissions();
+        permissions.set_mode(0o244); // -w-r--r-- permission, with writeonly for the user
+        fs::set_permissions(file_path, permissions).expect("Failed to set permissions");
+
+        // Prepare the application
+        let args: Vec<String> = Vec::new();
+        let mut app = App::new(&args);
+        app.set_app_mode(AppMode::Command(CommandMode::Read));
+        app.command_buffer = Buffer::with_contents(file_path_string.clone());
+
+        app.open_file();
+    }
+
+    #[test]
+    #[should_panic(expected = "Unable to write file")]
+    fn save_readonly_file_fail() {
+        // Doesn't return io::Result<()> like other tests because should_panic must return unit
+        // Prepare the files
+        let dir = tempdir().expect("Failed to create tempdir");
+        let file_path = dir.path().join("temp.txt");
+        let file_path_string = file_path.to_string_lossy().into_owned();
+        let file = File::create(file_path.clone()).expect("Failed to create file");
+
+        // Make the file become readonly
+        let metadata = file.metadata().expect("Failed to get metadata");
+        let mut permissions = metadata.permissions();
+        permissions.set_readonly(true);
+        fs::set_permissions(file_path, permissions).expect("Failed to set permissions");
+
+        // Prepare the application
+        let args: Vec<String> = Vec::new();
+        let mut app = App::new(&args);
+        app.set_app_mode(AppMode::Command(CommandMode::Write));
+        app.command_buffer = Buffer::with_contents(file_path_string.clone());
+        app.buffer = Buffer::with_contents(String::from("Testing Write, this should fail!"));
+
+        app.save_file();
     }
 
     #[test]
